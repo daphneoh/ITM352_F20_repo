b0a8e90b2eb12b0007b4f896bfa332a1d6d5ea1e
daphneoh <daphneoh@hawaii.edu>
2020-10-01 11:51:36 -1000

    lab8

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -3,10 +3,12 @@ age = 21;
 
 while (age_count < age) {
     if(age_count >= age/2){
-        console.log("I'm old");
-        break;
+        console.log("Don’t ask how old I am!");
+        process.exit();
     }
+    else {
     console.log(`age ${age_count}`);
+    }
     age_count++;
 }
 console.log(`Daphne is ${age_count} years old`);
---
+++ Lab8/Ex3.js
@@ -0,0 +1,7 @@
+ //starts the age counter.
+age = 21; 
+
+for (var age_count = 50; age_count > age; age_count--) {
+    console.log(`age ${age_count}`);
+}
+console.log(`Daphne is ${age_count} years old`);
