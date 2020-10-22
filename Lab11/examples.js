0f0fb940dd73c38597df3e59d35b181b0e47495f
daphneoh <daphneoh@hawaii.edu>
2020-10-22 11:55:59 -1000

    lab12

--- Lab11/examples.js
+++ Lab11/examples.js
@@ -16,7 +16,7 @@ function isNonNegIntString(string_to_check, returnErrors = false) {
     if (string_to_check < 0) errors.push('Negative value!'); // Check if it is non-negative
     if (parseInt(string_to_check) != string_to_check) errors.push('Not an integer!'); // Check that it is an integer
 
-    return returnErrors ? errors : (errors.length == 0);
+    return returnErrors ? errors : ((errors.length > 0) ? false : true);
 }
 
 function callback(part,i){
