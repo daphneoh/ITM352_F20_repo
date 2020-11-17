3b17c87e104a57c409b8bd78089f9b78ea2a108c
daphneoh <daphneoh@hawaii.edu>
2020-11-17 11:51:33 -1000

    assignment2

--- Lab14/Ex1.js
+++ Lab14/Ex1.js
@@ -11,12 +11,52 @@ if (fs.existsSync(filename)) {
     console.log(`user_data.json has ${stats['size']} characters`);
     var data = fs.readFileSync(filename, 'utf-8');
     var users_reg_data = JSON.parse(data);
+
+  
+
+
 } else {
     console.log(`ERR: ${filename} does not exits!!!`);
 }
 
 //Process login. Ex3 Lab 14
 app.use(myParser.urlencoded({ extended: true }));
+
+app.get("/register", function (request, response) {
+    // Give a simple register form
+    str = `
+<body>
+<form action="process_register" method="POST">
+<input type="text" name="username" size="40" placeholder="enter username" ><br />
+<input type="password" name="password" size="40" placeholder="enter password"><br />
+<input type="password" name="repeat_password" size="40" placeholder="enter password again"><br />
+<input type="email" name="email" size="40" placeholder="enter email"><br />
+<input type="submit" value="Submit" id="submit">
+</form>
+</body>
+    `;
+    response.send(str);
+ });
+
+ app.post("/process_register", function (request, response) {
+    // process a simple register form
+    // validate the reg info
+
+    // if all data is valid, write to the user_data_filename and send to invoice. 
+      //add example new user reg info.
+      username = request.body.username; //what user typed in. Then need to check that it is not taken.
+      users_reg_data[username] = {};
+      users_reg_data[username].password = request.body.password;
+      users_reg_data[username].email = request.body.email;
+    
+      // write updated object to user_data_filename. 
+      reg_info_str = JSON.stringify(users_reg_data);
+      fs.writeFileSync(filename, reg_info_str);
+
+    
+
+ });
+
 
 app.get("/login", function (request, response) {
     // Give a simple login form
@@ -30,14 +70,15 @@ app.get("/login", function (request, response) {
 </body>
     `;
     response.send(str);
- });
+});
 
 app.post("/process_login", function (request, response) {
     // Process login form POST and redirect to logged in page if ok, back to login page if not
     console.log(request.body);
     //if user exits, get their password
-    if(typeof users_reg_data[request.body.username] != 'undefined'){
-        if(request.body.password == users_reg_data[request.body.username].password){
+    //convert all to lower case when comparing 
+    if (typeof users_reg_data[request.body.username] != 'undefined') {
+        if (request.body.password == users_reg_data[request.body.username].password) {
             response.send(`Thank you ${request.body.username} for logging in.`);
         } else {
             response.send(`Hey! ${request.body.username} does not match what we have for you!`);
